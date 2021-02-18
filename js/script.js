db2c236f9645566461c801dd3a8b008f3207cb63
FrensysThomas87 <delta873@gmail.com>
2021-02-18 12:48:22 +0100

    creato oggetto headerNavbar e rinominata cartella script in js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+new Vue({
+ el: '#app',
+ data:{
+   headerNavbar:
+     {
+       clockIcon:'fas fa-clock',
+       openingTime: 'Open Hours Mon-Sat-9:00-18:00',
+       telephoneIcon:'fas fa-phone',
+       telephoneNumber: '+1(305) 1234-5678',
+       messageIcon:'fas fa-envelope',
+       mailAdress:'hello@example.com',
+       facebookIcon: 'fab fa-facebook-f',
+       twitterIcon:'fab fa-twitter',
+       linkedinIcon:'fab fa-linkedin-in'
+     },
+
+ },
+});
+Vue.config.devtools = true;
